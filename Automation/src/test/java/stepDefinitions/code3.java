ffd6d0a1f5dd825dcfd0ba35b3af6e5c4a901069
Brahaspati Mishra <Brahaspatimishra@outlook.com>
2020-08-31 16:09:49 +0530

    updated imports

--- Automation/src/test/java/stepDefinitions/code3.java
+++ Automation/src/test/java/stepDefinitions/code3.java
@@ -1,10 +1,7 @@
 package stepDefinitions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class code3 {
 
@@ -12,14 +9,14 @@ public class code3 {
 	public static void getValue(List<ArrayList<String>> customer) {
 		//customer.get(0).forEach(cust->);
 		//customer.get(0).stream().filter(k -> k.split("likes")).sorted().forEach(System.out::print);
-		customer.get(0).stream().map(k -> k.split("likes")[0].trim()+k.split("likes")[1].trim()).forEach(System.out::println);;
+		customer.get(0).stream().map(k -> k.split("likes")[0].trim()+k.split("likes")[1].trim()).forEach(System.out::println);
 		//System.out.println(customer);
 	}
 	
 	
 	public static void main(String[] args) {
-		List<ArrayList<String>> customer = new ArrayList<ArrayList<String>>();
-		ArrayList<String> toBeAdded = new ArrayList<String>();
+		List<ArrayList<String>> customer = new ArrayList<>();
+		ArrayList<String> toBeAdded = new ArrayList<>();
 		toBeAdded.add("abc likes cds");
 		toBeAdded.add("123 likes asd");
 		toBeAdded.add("cds likes abc");
